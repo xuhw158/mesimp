df6f55a749a69208bd7137f15fef7f78b6d7a185
Admin <Admin@192.168.1.100>
2017-10-15 13:16:52 +0800

    delete

--- mesimp-git/src/main/webapp/static/js/resize.js
+++
@@ -1,4 +0,0 @@
-function resizeIndex(){
-	var otherHeight = $("#index_header").outerHeight() + $("#index_banner").outerHeight() + $("#index_footer").outerHeight();
-	$("#index_main").height($(window).height() - otherHeight);
-}
